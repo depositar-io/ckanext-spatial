0d9d3a09b58037a16c6b2b43ce2b844f650b99c3
Sol Lee <u103133.u103135@gmail.com>
2015-04-09 16:27:19 +0800

    add shp preview

--- setup.py
+++ setup.py
@@ -32,8 +32,10 @@ setup(
     spatial_query=ckanext.spatial.plugin:SpatialQuery
     wms_view=ckanext.spatial.nongeos_plugin:WMSView
     geojson_view=ckanext.spatial.nongeos_plugin:GeoJSONView
+    shp_view=ckanext.spatial.nongeos_plugin:SHPView
     wms_preview=ckanext.spatial.nongeos_plugin:WMSPreview
     geojson_preview=ckanext.spatial.nongeos_plugin:GeoJSONPreview
+    shp_preview=ckanext.spatial.nongeos_plugin:SHPPreview
     cswserver=ckanext.spatial.plugin:CatalogueServiceWeb
     spatial_harvest_metadata_api=ckanext.spatial.plugin:HarvestMetadataApi
 
